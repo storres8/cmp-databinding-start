75b779a9931e717d2a3ce9cc8b3af319120d4bf7
storres8 <stevetorres7562@gmail.com>
2019-06-22 15:42:45 -0400

    used event emitting to send data from child component to parent component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [
     { type: "server", name: "Test-Server", content: "Just a Test!" }
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "server",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: "blueprint",
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
