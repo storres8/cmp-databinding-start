a9fc83d145987e3f8775f4baf8300aea955332d0
storres8 <stevetorres7562@gmail.com>
2019-06-22 15:07:28 -0400

    used property binding to send data from parent to child component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { type: "server", name: "Test-Server", content: "Just a Test!" }
+  ];
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-server-element',
-  templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  selector: "app-server-element",
+  templateUrl: "./server-element.component.html",
+  styleUrls: ["./server-element.component.css"]
 })
 export class ServerElementComponent implements OnInit {
+  // @Input is a method that allows you to accept an incoming object from a parent component.
+  // passing in a string to input you can customize the name of the parent binding.
+  @Input("svrElement") element: {
+    type: string;
+    name: string;
+    content: string;
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
